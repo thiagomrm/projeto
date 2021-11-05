47be965f0ad9d2440fbdb49c27f0ea55e7704796
Thiago Mesquita <thiagomrm@yahoo.com.br>
2021-11-05 08:30:38 -0300

    Home, navigation

--- frontend/src/Routes.tsx
+++ frontend/src/Routes.tsx
@@ -1,7 +1,7 @@
 import Dashboard from "pages/Dashboard";
 import Home from "pages/Home";
 import { Switch } from "react-router-dom";
-import { BrowserRouter, Route } from "react-router-dom";
+import { BrowserRouter, Route} from "react-router-dom";
 
 
 const Routes = () => {
